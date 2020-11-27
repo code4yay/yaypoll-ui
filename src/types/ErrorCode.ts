7d1af9f45629837b9a1a498cc739371c54822673
hota1024 <hotalog@hota1024.com>
2020-11-27 21:45:28 +0900

    🏷️ Add ErrorCode type

---
+++ src/types/ErrorCode.ts
@@ -0,0 +1,11 @@
+/**
+ * エラーコード
+ */
+export type ErrorCode =
+  | 'password_is_empty'
+  | 'email_is_empty'
+  | 'passowrd_or_email_is_invalid'
+  | 'failed_to_find_user'
+  | 'this_user_has_already_voted'
+  | 'failed_to_find_work'
+  | 'failed_to_save'
