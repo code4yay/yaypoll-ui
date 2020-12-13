39a7e1ea473be681a06a80bcdd36629a11701028
hota1024 <hotalog@hota1024.com>
2020-12-13 21:38:08 +0900

    💄 ログイン画面を追加

---
+++ src/pages/login.tsx
@@ -0,0 +1,133 @@
+import { NextPage } from 'next'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Collapse,
+  Container,
+  Divider,
+  makeStyles,
+  TextField,
+  Typography,
+  LinearProgress,
+} from '@material-ui/core'
+import { useState } from 'react'
+import { useApi } from '../hooks/useApi'
+import { useRouter } from 'next/dist/client/router'
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    marginTop: theme.spacing(8),
+  },
+}))
+
+/**
+ * LoginPage component.
+ */
+export const LoginPage: NextPage = () => {
+  const classes = useStyles()
+  const [page, setPage] = useState(0)
+  const [email, setEmail] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  const [token, setToken] = useState('')
+
+  const api = useApi()
+  const router = useRouter()
+
+  const onNextClicked = async () => {
+    setError('')
+    setLoading(true)
+    const result = await api.requestLogInToken(email)
+    setLoading(false)
+
+    if (result.errors.length === 0) {
+      setPage(1)
+    } else {
+      setError(result.errors[0].message)
+    }
+  }
+
+  const onLoginClicked = async () => {
+    setError('')
+    setLoading(true)
+    const result = await api.login(email, token)
+    setLoading(false)
+
+    if (result.errors.length === 0) {
+      router.push('/')
+    } else {
+      setError(result.errors[0].message)
+    }
+  }
+
+  return (
+    <>
+      <Container component="main" maxWidth="xs">
+        <Card className={classes.card}>
+          {loading && <LinearProgress />}
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              ログイン
+            </Typography>
+          </CardContent>
+          <Divider />
+          <Collapse in={page === 0}>
+            <CardContent>
+              <TextField
+                error={!!error}
+                helperText={error}
+                label="メールアドレス"
+                variant="filled"
+                fullWidth
+                value={email}
+                onChange={({ target: { value } }) => setEmail(value)}
+                disabled={loading}
+              />
+            </CardContent>
+            <CardActions>
+              <Button
+                color="primary"
+                variant="contained"
+                disableElevation
+                fullWidth
+                onClick={onNextClicked}
+                disabled={loading}
+              >
+                次へ
+              </Button>
+            </CardActions>
+          </Collapse>
+          <Collapse in={page === 1}>
+            <CardContent>
+              <TextField
+                error={!!error}
+                helperText={error}
+                label="トークン"
+                variant="filled"
+                fullWidth
+                onChange={({ target: { value } }) => setToken(value)}
+              />
+            </CardContent>
+            <CardActions>
+              <Button onClick={() => setPage(0)}>戻る</Button>
+              <Button
+                color="primary"
+                variant="contained"
+                disableElevation
+                onClick={onLoginClicked}
+                style={{ marginLeft: 'auto' }}
+                disabled={loading}
+              >
+                ログイン
+              </Button>
+            </CardActions>
+          </Collapse>
+        </Card>
+      </Container>
+    </>
+  )
+}
+
+export default LoginPage
